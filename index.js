a640db055ef95f5455e3fadb913478144a775b3a
zulaigahbenjamin <130163047+zulaigahbenjamin@users.noreply.github.com>
2023-05-16 15:33:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let myName = 'Zulaigah';
+let mySurname = 'Benjamin';
+
+document.write('My name is ' + myName + '.' + 'My surname is ' + mySurname + '.' );
+
+
+let numberVar = 5;
+document.write(numberVar*5);
+
